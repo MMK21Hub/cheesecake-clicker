968a112a273cc9febf86ea6529f57167bccd2ba8
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-11-13 00:00:01 +0000

    make it count cheesecakes

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,9 +1,32 @@
+import { store } from "voby"
+
+export const gameData = store(
+  JSON.parse(localStorage.getItem("cheesecake-data") || "{}") || {
+    cheesecakes: 0,
+  }
+)
+
+export function saveGame() {
+  localStorage.setItem("cheesecake-data", JSON.stringify(gameData))
+}
+
 function Game(): JSX.Element {
   return (
     <>
+      <div class="top-area">
+        <div class="cheesecake-count">
+          <span>{() => gameData.cheesecakes} cheesecakes</span>
+        </div>
+      </div>
       <div class="middle-area">
         <div class="big-cheesecake-container">
-          <button class="big-cheesecake">
+          <button
+            class="big-cheesecake"
+            onClick={() => {
+              gameData.cheesecakes += 1
+            }}
+            type="button"
+          >
             <img
               src="/assets/cheesecake-temp-design.png"
               alt="Big Cheesecake"
